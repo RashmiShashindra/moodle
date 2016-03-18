4b67e35c3f35efef29dd4ed4c57e96db53944c7c
unknown <RashmiM@Rashmi-M.Zone24x7.lk>
2016-03-18 17:26:05 +0530

    Test case writing Part 01

--- src/test/java/com/automation/pageobjects/LoginPage.java
+++ src/test/java/com/automation/pageobjects/LoginPage.java
@@ -3,6 +3,7 @@ package com.automation.pageobjects;
 import com.automation.browser.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 /**
  * Created by RashmiM on 3/15/2016.
@@ -12,19 +13,34 @@ public class LoginPage extends BasePage{
     By username = By.id("login_username");
     By password = By.id("login_password");
     By login = By.xpath("//*[@id='login']/div[4]/input");
+    By loginErrorMessage = By.xpath("//span[@class='error']");
 
     public LoginPage(WebDriver driver){
         super(driver);
     }
 
     public LandingPage step_Successful_User_Login(String uname,String pword){
-        action_EenterUsername(uname);
+        action_EnterUsername(uname);
         action_EnterPassword(pword);
         clickLoginButton();
         return new LandingPage(driver);
     }
 
-    private void action_EenterUsername(String name){
+    public LoginPage step_Unsuccessful_User_Login(String uname, String pwd){
+        action_EnterUsername(uname);
+        action_EnterPassword(pwd);
+        clickLoginButton();
+        return this;
+    }
+
+    public LoginPage step_Verify_Error_Message(String errorMessage) throws InterruptedException {
+        Thread.sleep(2000);
+        Assert.assertTrue(driver.findElement(loginErrorMessage).getText().equals(errorMessage));
+        System.out.println("Validated error message successfully!");
+        return this;
+    }
+
+    private void action_EnterUsername(String name){
         driver.findElement(username).sendKeys(name);
         System.out.println("Entered username Successfully");
     }
@@ -38,5 +54,6 @@ public class LoginPage extends BasePage{
         driver.findElement(login).click();
         System.out.println("Clicked Login button Successfully");
     }
+
 
 }
--- src/test/java/com/automation/testcases/LoginTest.java
+++ src/test/java/com/automation/testcases/LoginTest.java
@@ -14,10 +14,10 @@ public class LoginTest extends BaseTest{
     }
 
     @Test
-    public void test_Invalid_User_login(){
+    public void test_Invalid_User_login() throws InterruptedException {
         new LoginPage(getDriver()).
-                step_Successful_User_Login("admin", "sandbox").
-                Check_DashboardProfile("Admin User");
+                step_Unsuccessful_User_Login("Sister","sandbox").
+                step_Verify_Error_Message("Invalid login, please try again");
     }
 
 }
